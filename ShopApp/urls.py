88448a422c463d8314e4f0fb84874a59bc1eaf9d
kimanii00 <samanthakimani06@gmail.com>
2025-02-19 13:25:45 +0300

    initial commit

---
+++ ShopApp/urls.py
@@ -0,0 +1,11 @@
+from tkinter.font import names
+
+from django.contrib import admin
+from django.urls import path
+from ShopApp import views
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home,name='home'),
+    path('gallery/', views.images,name='gallery'),
+    path('about/', views.about,name='about'),
+]
---
+++ ShopApp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request,'index.html')
+
+def images(request):
+    return render(request,'gallery.html')
+
+def about(request):
+    return render(request,'about.html')
